d72f532544319752e4e9321f1a697e24e998537b
Jason Chen <chenjason971@gmail.com>
2022-06-14 10:03:28 -0500

    Add project files.

---
+++ Odd_Or_Even/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Odd_Or_Even
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
